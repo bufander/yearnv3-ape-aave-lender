9ef98f53993150e4342fc0b313fccb53f57cb3f6
bufander <bufanderbufander@gmail.com>
2022-11-09 13:07:01 +0100

    feat: updated vaultv3 version

--- tests/conftest.py
+++ tests/conftest.py
@@ -42,17 +42,13 @@ def atoken():
 
 @pytest.fixture(scope="session")
 def create_vault(project, gov):
-    def create_vault(
-        asset,
-        governance=gov,
-        deposit_limit=MAX_INT
-    ):
+    def create_vault(asset, governance=gov, deposit_limit=MAX_INT):
         vault = gov.deploy(
             project.dependencies["yearn-vaults"]["master"].VaultV3,
             asset,
             "VaultV3",
             "AV",
-            governance
+            governance,
         )
         # set vault deposit
         vault.set_deposit_limit(deposit_limit, sender=gov)
